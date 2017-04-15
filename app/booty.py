19da7b24bc009775053ba42995e497cebdc1d920
PrettyPinkPonyOfDeath <martin.hauke.jr@gmail.com>
2017-04-15 13:07:22 +0200

    Put logic in separate file for better testing; Added !ping command;

--- app/booty.py
+++ app/booty.py
@@ -1,6 +1,7 @@
 # This is bootybot
 import discord
 import asyncio
+import commands as ec
 from discord.ext import commands
 from secrets import TOKEN
 
@@ -16,11 +17,31 @@ async def on_ready():
     print('--------')
 
 
+@bootybot.event
+async def on_message(message):
+    """Does not do anything except forwarding input to commands, yet"""
+    ec.print_message(message)
+    await bootybot.process_commands(message)
+
+
 @bootybot.command()
 async def hello(*args):
     """Replies with 'Hello, World!'."""
-    print(args)
-    await bootybot.say("Hello, World!")
+    await bootybot.say(ec.hello(args))
+
+
+@bootybot.command()
+async def echo(*, message: str):
+    """Repeats the user's input."""
+    await bootybot.say(ec.echo(message))
+
+
+@bootybot.command()
+async def ping():
+    """Returns 'pong!'
+ 
+    Can be used to check the connection."""
+    await bootybot.say(ec.ping())
 
 
 bootybot.run(TOKEN)
---
+++ app/commands.py
@@ -0,0 +1,36 @@
+# Functionality of commands will be implemented here so we are able to write
+# unit tests without really complicated setups
+
+
+def hello(*args):
+    """Prints 'Hello, World!'"""
+    return "Hello, World!"
+
+
+def echo(*msg):
+    """Returns the Input"""
+    return msg
+
+
+def print_message(msg):
+    """Prints a message"""
+    print(message_to_string(msg))
+
+
+def message_to_string(msg):
+    """Converts a message to String."""
+    msg_time = msg.timestamp.strftime('%Y-%m-%d %H:%M:S')
+    msg_string = "[ " + msg_time + " ]: "
+    msg_string += msg.content
+
+    return msg_string.encode('utf-8')
+
+
+def list(*args):
+    """Manages lists"""
+    pass
+
+
+def ping():
+    """Returns 'pong!'"""
+    return "pong!"

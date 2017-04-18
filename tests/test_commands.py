c432f607fd83d5514f19d2d82150ac9619947e37
PrettyPinkPonyOfDeath <martin.hauke.jr@gmail.com>
2017-04-18 22:22:51 +0200

    Added some tests; Realized testing this will be hard;

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -11,6 +11,10 @@ class TestCommands(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(commands.hello(), "Hello, World!")
 
+    def test_hello_with_args(self):
+        self.assertEqual(commands.hello(["arg1", "arg2", "arg3"]),
+                         "Hello, World!")
+
     def test_echo(self):
         teststring = "This should be echoed"
         self.assertEqual(commands.echo(teststring), teststring)
